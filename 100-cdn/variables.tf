c7dca6aaa66e03b08e4514ac7cb4cb670b241293
vamsi <vamsibattu3015@gmail.com>
2025-06-30 11:37:04 +0530

    committed

---
+++ 100-cdn/variables.tf
@@ -0,0 +1,21 @@
+variable "project_name" {
+  type    = string
+  default = "Expense"
+}
+variable "environment" {
+  type    = string
+  default = "Dev"
+
+}
+
+variable "common_tags" {
+  default = {
+    Project     = "Expense"
+    Environment = "Dev"
+    Terraform   = "True"
+  }
+}
+
+variable "zone_name" {
+    default = "daws81s.icu"
+}
